27369e486433b66a0e35c3832e8cff50b51ea544
Or Pravda <orpravda@Ors-MacBook-Pro.lan>
2024-11-15 16:51:59 +0200

    num 3 is working

--- Cheers.java
+++ Cheers.java
@@ -7,8 +7,6 @@ public class Cheers {
                 String allUeerWord = "";
 
                 for(int i=0 ; i < cheerWord.length() ; i++ ){
-                       // int s = Integer.parseInt(cheerWord.charAt(i));
-                        //cheerWord.indexOf(cheerWord.charAt(i)); // the index of the letter I'm checking
                         char currentChar = Character.toUpperCase(cheerWord.charAt(i)); // the uppercase of the letter I'm changing
                         allUeerWord = allUeerWord + currentChar;
                         boolean check = false;
@@ -21,19 +19,16 @@ public class Cheers {
                             }
                         }   
                         if(!check){
-                                System.out.println("Give me a " + currentChar + ": " + currentChar + "!");
+                                System.out.println("Give me a  " + currentChar + ": " + currentChar + "!");
                         }                         
                 }
-
-               // if (s =='A' || s =='E' || s =='F' || s =='H' || s =='I' || s =='L' || s =='M' || s =='N' || s =='O' || s ==  'R'|| s =='S' || s =='X' ||
-              //  s =='a' || s =='e' || s =='f' || s =='h' || s =='i' || s =='l' || s =='m' || s =='n' || s =='o' || s ==  'r'|| s =='s' || s =='x')
 
                System.out.println("What does that spell?");
                 for(int i=0; i< repeat ; i++ ){
                         System.out.println(allUeerWord + "!!!");
                 }
-               // Expected:%0AGive me an E: E!%0AGive me an X: X!%0AGive me a  C: C!%0AGive me an E: E!%0AGive me an L: L!%0AWhat does that spell?%0AEXCEL!!!%0AEXCEL!!!%0A
-                ///Actual:%0AGive me an E: E!%0AGive me an X: X!%0AGive me an C: C!%0AGive me an E: E!%0AGive me an L: L!%0AWhat does that spell?%0AEXCEL!!!%0AEXCEL!!!
-        
+                //Expected:%0AGive me a  C: C!%0AGive me an H: H!%0AGive me an E: E!%0AGive me a  C: C!%0AGive me a  K: K!%0AWhat does that spell?%0A
+                //Actual:%0AGive me a C: C!%0AGive me an H: H!%0AGive me an E: E!%0AGive me a C: C!%0AGive me a K: K!%0AWhat does that spell?
+
         }
 }
