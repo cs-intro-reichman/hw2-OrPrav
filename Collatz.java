effeda0ea70b6b4785c41a0b07e604bc81eac3d0
Or Pravda <orpravda@Ors-MacBook-Pro.lan>
2024-11-15 21:42:12 +0200

    its  Night already

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,53 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		 int seed = Integer.parseInt(args[0]);
+		 String mod = args[1];
+		 int newSeed = 1;
+		 int line = 1;
+		 int count = 0;
+
+		 if (mod.equals("v")) {
+			
+			if(newSeed % 2 == 0){
+				System.out.print(newSeed + " ");
+				newSeed /= 2;
+				count++;
+			}
+			else{
+				System.out.print(newSeed + " ");
+				newSeed = (newSeed*3) + 1;
+				count++;
+			}
+
+			for(int i = 0; i != seed; i++) { 
+			while (newSeed != 1) {
+			if(newSeed % 2 == 0){
+				System.out.print(newSeed + " ");
+				newSeed /= 2;
+				count++;
+			}
+			else{
+				System.out.print(newSeed + " ");
+				newSeed = (newSeed*3) + 1;
+				count++;
+			}
+			}
+			count++;
+			System.out.print(newSeed + " ");
+			System.out.print("(" + count + ")");
+			System.out.println();
+			count = 0;
+			line ++;
+			newSeed = line;
+			}
+			System.out.println("Every one of the first " + seed + " " + "hailstone sequences reached 1");
+		}
+		else{
+			System.out.println("Every one of the first " + seed+  " hailstone sequences reached 1");
+		}
+	
+		
+
 	}
 }
