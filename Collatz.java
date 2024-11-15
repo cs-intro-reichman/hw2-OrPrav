ba52069802d80c1a7c3a0168dc8068effee34f11
Or Pravda <orpravda@Ors-MacBook-Pro.lan>
2024-11-15 22:14:41 +0200

    its  Night already

--- Collatz.java
+++ Collatz.java
@@ -41,11 +41,12 @@ public class Collatz {
 			line ++;
 			newSeed = line;
 			}
-			System.out.println("Every one of the first " + seed  + "hailstone sequences reached 1.");
+			System.out.println("Every one of the first " + seed  + " hailstone sequences reached 1.");
 		}
 		if(mod.equals("c") || mod.equals("C")) {
 		System.out.println("Every one of the first " + seed +  " hailstone sequences reached 1.");
 		}
-		
+	
+
 	}
 }
