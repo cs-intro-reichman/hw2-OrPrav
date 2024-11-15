2e48002d7eb4333d3acc2dc417669ad731172ec6
Or Pravda <orpravda@Ors-MacBook-Pro.lan>
2024-11-15 18:42:34 +0200

    num 4 is working

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,28 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		int input = Integer.parseInt(args[0]);
+		double approximated = 0;
+
+		for (int i = 0; i < input; i++) {
+            double term = 1.0 / (2 * i + 1);  // 1/(2n + 1)
+            if (i % 2 == 0) {
+                approximated += term; 
+            } else {
+                approximated -= term;  
+            }
+        }
+
+		approximated= approximated*4;
+			if (input !=0){
+				System.out.println("pi according to Java: " + Math.PI);
+				System.out.println("pi, approximated: " + approximated);
+				} 
+				else{
+				System.out.println("pi according to Java: " + Math.PI);
+				System.out.println("the input is 0");
+				}
+		
+
 	}
 }
