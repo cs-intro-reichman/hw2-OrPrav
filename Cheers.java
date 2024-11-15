a2d7347d2a50376385ecde0bb505731ec38f5089
Or Pravda <orpravda@Ors-MacBook-Pro.lan>
2024-11-15 16:08:56 +0200

    num 3 is working

--- Cheers.java
+++ Cheers.java
@@ -1,22 +1,36 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-                String letters = args[0];
+                String cheerWord = args[0];
                 int repeat = Integer.parseInt(args[1]);
+                String letters = "AEFHILMNORSXaefhilmnorsx";
+                String allUeerWord = "";
 
-                for(int i=0 ; i < letters.length() ; i++ ){
-                        char s = letters.charAt(i);
-                        if (s =='A' || s =='E' || s =='F' || s =='H' || s =='I' || s =='L' || s =='M' || s =='N' || s =='O' || s ==  'R'|| s =='S' || s =='X' ||
-                        s =='a' || s =='e' || s =='f' || s =='h' || s =='i' || s =='l' || s =='m' || s =='n' || s =='o' || s ==  'r'|| s =='s' || s =='x'){
-                        System.out.println("Give me an " + s + ": " + s + "!");  
-                        }else{
-                        System.out.println("Give me a " + s + ": " + s + "!");  
-                        }
+                for(int i=0 ; i < cheerWord.length() ; i++ ){
+                        int s = cheerWord.indexOf(cheerWord.charAt(i)); // the index of the letter I'm checking
+                        char c = Character.toUpperCase(cheerWord.charAt(i)); // the uppercase of the letter I'm changing
+                        allUeerWord = allUeerWord + c;
+                        boolean check = false;
+
+                        for(int j=0 ; j< 12 ; j++){
+                            if (s == letters.indexOf(letters.charAt(j))) {
+                                System.out.println("Give me an " + c + ": " + c + "!");  
+                                check = true; // this letter is getting an
+                                break;
+                            }
+                        }   
+                        if(!check){
+                                System.out.println("Give me a " + s + ": " + s + "!");
+                        }                         
                 }
+
+               // if (s =='A' || s =='E' || s =='F' || s =='H' || s =='I' || s =='L' || s =='M' || s =='N' || s =='O' || s ==  'R'|| s =='S' || s =='X' ||
+              //  s =='a' || s =='e' || s =='f' || s =='h' || s =='i' || s =='l' || s =='m' || s =='n' || s =='o' || s ==  'r'|| s =='s' || s =='x')
+
                System.out.println("What does that spell?");
                 for(int i=0; i< repeat ; i++ ){
-                        System.out.println(letters + "!!!");
+                        System.out.println(allUeerWord + "!!!");
                 }
-
+        
         }
 }
